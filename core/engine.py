ae12d70ae7b0060b7f7a2150cd35eb222d0de8c3
Mag329 <92212582+Mag329@users.noreply.github.com>
2024-05-18 19:25:00 +0300

    Comment engine (#1)
    * comment str
    
    * comment str
    
    * comment str
    
    * delete str

--- core/engine.py
+++ core/engine.py
@@ -18,7 +18,9 @@ async def hunger(id, auto=True, index=None):
         
     session.commit()
     session.close()
-    
+
+    # smth edit
+
 
 async def sadness(id, auto=True, index=None):
     session = Session()
